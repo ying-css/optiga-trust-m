f631fedfc7215c1a1b364ee5ef9d6d632afdf2fb
ying-css <ying.ye@infineon.com>
2026-07-16 15:32:07 +0800

    1)Remove the support for mbedtls2.x and mbedtls3.x

--- tests/core.cmake
+++ tests/core.cmake
@@ -10,15 +10,10 @@ include_directories(
     ${PROJECT_SOURCE_DIR}/../include/comms
     ${PROJECT_SOURCE_DIR}/../include/cmd
     ${PROJECT_SOURCE_DIR}/../include/ifx_i2c
-if(BUILD_MBEDTLS_3)
-    ${PROJECT_SOURCE_DIR}/../external/mbedtls-3.x/include
-    ${PROJECT_SOURCE_DIR}/../external/mbedtls-3.x/include/mbedtls
-    ${PROJECT_SOURCE_DIR}/../external/mbedtls-3.x/include/psa
-else()
-    ${PROJECT_SOURCE_DIR}/../external/mbedtls/include
-    ${PROJECT_SOURCE_DIR}/../external/mbedtls/include/mbedtls
-    ${PROJECT_SOURCE_DIR}/../external/mbedtls/include/psa
-endif()
+    ${PROJECT_SOURCE_DIR}/../external/mbedtls-4.x/include
+    ${PROJECT_SOURCE_DIR}/../external/mbedtls-4.x/include/mbedtls
+    ${PROJECT_SOURCE_DIR}/../external/mbedtls-4.x/tf-psa-crypto/include
+    ${PROJECT_SOURCE_DIR}/../external/mbedtls-4.x/tf-psa-crypto/include/psa
     ${PROJECT_SOURCE_DIR}/../extras/pal/libusb/include
     ${PROJECT_SOURCE_DIR}/../tests
 )
@@ -30,9 +25,5 @@ aux_source_directory(${PROJECT_SOURCE_DIR}/../src/comms SRC_COMMS_FILES)
 aux_source_directory(${PROJECT_SOURCE_DIR}/../src/comms/ifx_i2c SRC_COMMS_IFX_I2C_FILES)
 aux_source_directory(${PROJECT_SOURCE_DIR}/../src/crypt SRC_CRYPT_FILES)
 aux_source_directory(${PROJECT_SOURCE_DIR}/../src/util SRC_UTIL_FILES)
-if(BUILD_MBEDTLS_3)
-aux_source_directory(${PROJECT_SOURCE_DIR}/../external/mbedtls-3.x/library MBEDTLS_FILES)
-else()
-aux_source_directory(${PROJECT_SOURCE_DIR}/../external/mbedtls/library MBEDTLS_FILES)
-endif()
+aux_source_directory(${PROJECT_SOURCE_DIR}/../external/mbedtls-4.x/library MBEDTLS_FILES)
 set(PAL_CRYPT_FILE ${PROJECT_SOURCE_DIR}/../extras/pal/pal_crypt_mbedtls.c)
